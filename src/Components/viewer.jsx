2ff9b11d1902592374f4153220fe9c83ef3e025a
Muhammad Bilal Aleem <30667628+brad911@users.noreply.github.com>
2023-11-30 20:24:20 +0500

    for display

--- src/Components/viewer.jsx
+++ src/Components/viewer.jsx
@@ -9,15 +9,23 @@ function Viewer({ uploadedFile }) {
     const initializeViewer = async () => {
       try {
         if (!instance && uploadedFile != null) {
-          console.log(uploadedFile,"mjhe nahi chalna shuru m")
           let obj=document.getElementById("file_upload")
           console.log(obj,"<== obj")
           instance = await WebViewer({
+            enableMeasurement: true,
             path: '/pdfjsexpress',
             initialDoc: "/files/sample.pdf", // Directly use the uploaded file from state
             extension: 'pdf', // Specify the file extension here if needed
             licenseKey: 'adjMD6yMNCUJMX75Uyda',
           }, viewer.current).then(instance=>{
+            instance.UI.enableFeatures([instance.UI.Feature.Measurement]);
+            instance.UI.disableElements(['toolbarGroup-Annotate']);
+            instance.UI.disableElements(['toolbarGroup-Shapes']);
+            instance.UI.disableElements(['toolbarGroup-Edit']);
+            instance.UI.disableElements(['toolbarGroup-FillAndSign']);
+            instance.UI.disableElements(['toolbarGroup-Insert']); // hides DOM element + disables shortcut
+            
+
             obj.addEventListener('change', () => {
               // Get the file from the input
               const file = obj.files[0];
