c76d20be3223e92d4d613027f4dd2e8cd5bc65a8
Muhammad Bilal Aleem <30667628+brad911@users.noreply.github.com>
2023-11-30 19:01:33 +0500

    added scale

--- src/Components/viewer.jsx
+++ src/Components/viewer.jsx
@@ -11,13 +11,28 @@ function Viewer({ uploadedFile }) {
         if (!instance && uploadedFile != null) {
           console.log(uploadedFile,"mjhe nahi chalna shuru m")
           let obj=document.getElementById("file_upload")
-          console.log(obj,"<== obj")
           instance = await WebViewer({
+            enableMeasurement: true,
             path: '/pdfjsexpress',
             initialDoc: "/files/sample.pdf", // Directly use the uploaded file from state
             extension: 'pdf', // Specify the file extension here if needed
             licenseKey: 'adjMD6yMNCUJMX75Uyda',
           }, viewer.current).then(instance=>{
+            //Enable Measurments
+            instance.UI.enableFeatures([instance.UI.Feature.Measurement]);
+            
+            //remove annotation
+            const { documentViewer, annotationManager } = instance.Core;
+            console.log(annotationManager,"<=== annotation manager");
+            documentViewer.addEventListener('annotationsLoaded', () => {
+              const annots = annotationManager.getAnnotationsList();
+              console.log(annots,"<=== annots")
+              // remove annotations
+              annotationManager.deleteAnnotations(annots);
+            });
+            //removed annotation block
+            
+            
             obj.addEventListener('change', () => {
               // Get the file from the input
               const file = obj.files[0];
